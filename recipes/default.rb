9fe3dd9e024f0fe5fd6064938661b62de9e8f164
root <root@ip-172-31-82-54.ec2.internal>
2019-06-09 19:21:08 +0000

    changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,16 @@
 package 'git' do
  action :install
 end
+git "/root/cheftest1" do
+  repository "git://github.com/Linz09/cheftest1.git"
+  reference "master"
+  action :sync
+end
+package "httpd" do
+  action :install
+end
+
+service  'httpd' do
+ action [ :enable, :start ]
+end
+
